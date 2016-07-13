84efe307e4f2717bab257c6724336360f1cebc36
danielnistreanu <daniel.nistreanu@evozon.com>
2016-07-13 15:11:00 +0300

    enter_username modified

--- src/test/java/com/selenium/pages/DictionaryPage.java
+++ src/test/java/com/selenium/pages/DictionaryPage.java
@@ -22,7 +22,7 @@ public class DictionaryPage extends PageObject {
     @FindBy(name="go")
     private WebElementFacade lookupButton;
 
-    public void enter_keywords(String keyword) {
+    public void enter_keywordsModifiedByAnotherUser(String keyword) {
         searchTerms.type(keyword);
     }
 
