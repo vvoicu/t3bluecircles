5d243cf8ded24db9d415d5a9a795cdec2dbb0e11
ioana24 <ioana.craita24@yahoo.com>
2016-07-15 16:13:04 -0700

    FilterRequests changed(using csv) A class for Constants

--- src/test/java/com/tests/FilterRequestTest.java
+++ src/test/java/com/tests/FilterRequestTest.java
@@ -9,19 +9,24 @@ import com.selenium.steps.LogInSteps;
 import com.selenium.steps.NavigationBarSteps;
 import com.selenium.steps.VacationMenuSteps;
 
-import net.serenitybdd.junit.runners.SerenityRunner;
+import junit.framework.TestCase;
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
-@RunWith(SerenityRunner.class)
+import net.thucydides.junit.annotations.UseTestDataFrom;
 
-public class FilterRequestTest {
-	@Managed(uniqueSession = true)
+
+@RunWith(SerenityParameterizedRunner.class)
+@UseTestDataFrom(value="src/test/resources/FilterRequest.csv")
+
+public class FilterRequestTest extends TestCase{
+	@Managed(uniqueSession = false)
 	public WebDriver webdriver;
 	@Steps
 	public LogInSteps loginSteps;
 	@Steps
 	public VacationMenuSteps vacationMenuSteps;
-	@Steps
+	@Steps 
 	public NavigationBarSteps navigationBarSteps;
 	@Steps
 	public FilterRequestSteps filterRequestSteps;
@@ -30,8 +35,9 @@ public class FilterRequestTest {
 	String password = "test";
 	String text = "VACATION";
 	String text1 = "My Request";
-	String checkBox= "Holiday";
-	String type="ALL";
+	String checkBox;
+	String checkBox2;
+	//String type="ALL";
 
 	@Test
 	public void selectTab() {
@@ -42,9 +48,10 @@ public class FilterRequestTest {
 		navigationBarSteps.clickOnTab(text);
 		vacationMenuSteps.clickOnMenu(text1);
 		filterRequestSteps.clickOnCheckBox(checkBox);
+		filterRequestSteps.clickOnCheckBox2(checkBox2);
 		filterRequestSteps.apply();
 		filterRequestSteps.selectNumber();
-		filterRequestSteps.verifyType(type);
+		//filterRequestSteps.verifyType(type);
 		
 		
 
---
+++ src/test/resources/Constants.java
@@ -0,0 +1,6 @@
+
+public class Constants {
+
+	public static final String CSV_PATH="/csv/";
+	public static final char ',';
+}
